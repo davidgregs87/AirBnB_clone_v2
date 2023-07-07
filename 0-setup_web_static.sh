c2e1b1337d74eb4f10311052d03c72e043bcf6e9
davidgregs87 <udohdavid27@gmail.com>
2023-07-07 05:33:04 +0000

    configure nginx before deploying

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -59,7 +59,7 @@ sudo chown -R ubuntu:ubuntu /data/
 # Re-configure our server with alias
 echo "server {
     listen 80 default_server;
-    listen [::]:80 default_sever;
+    listen [::]:80 default_server;
     root /var/www/html;
     index index.html index.htm;
     location / {
