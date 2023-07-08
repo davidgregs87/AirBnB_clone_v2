c4e98b220a13cf8c95f3162cbe010651718fa86c
davidgregs87 <udohdavid27@gmail.com>
2023-07-08 19:13:11 +0000

    made some changes for task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,16 +8,13 @@ import os
 def do_pack():
     """pack up all our files into an archive"""
     date = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
-    archive_path = 'web_static_{}.tgz'.format(date)
-    print('Packing web_static to versions/{}'.format(archive_path))
-    if not os.path.exists('versions'):
-        local('mkdir versions')
-
-    extract = local('tar -cvzf versions/{} web_static/'.format(archive_path))
-    if extract.succeeded:
-        full_path = 'versions/{}'.format(archive_path)
-        size = os.path.getsize(full_path)
-        print('web_static packed: {} -> {}Bytes'.format(full_path, size))
-        return full_path
-    else:
-        return None
+    archive_path = 'versions/web_static_{}.tgz'.format(date)
+    print('Packing web_static to {}'.format(archive_path))
+    if os.path.exists(archive_path) is True:
+        return True
+    local('mkdir versions')
+    extract = local('tar -cvzf {} web_static'.format(archive_path))
+    if extract.failed:
+        return archive_path
+    size = os.path.getsize(archive_path)
+    print('web_static packed: {} -> {}Bytes'.format(archive_path, size))
