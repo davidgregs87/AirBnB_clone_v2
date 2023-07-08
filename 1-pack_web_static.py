caf59ed798c1f1240c2d10572bf53ce6dd12e705
davidgregs87 <udohdavid27@gmail.com>
2023-07-08 15:29:35 +0000

    [fixed] pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,18 +4,19 @@ from fabric.api import *
 from datetime import datetime
 import os
 
+
 def do_pack():
-	"""pack up all our files into an archive"""
-	date = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
-	archive_path = 'web_static_{}.tgz'.format(date)
-	print('Packing web_static to versions/{}'.format(archive_path))
-	if not os.path.exists('versions'):
-                local('mkdir versions')
-	
-	extract = local('tar -cvzf versions/{} web_static/'.format(archive_path))
-	if extract.succeeded:
-		size = os.path.getsize('versions/{}'.format(archive_path))
-		print('web_static packed: {} -> {}Bytes'.format(archive_path, size))
-		return archive_path
-	else:
-		return None 
+    """pack up all our files into an archive"""
+    date = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
+    archive_path = 'web_static_{}.tgz'.format(date)
+    print('Packing web_static to versions/{}'.format(archive_path))
+    if not os.path.exists('versions'):
+        local('mkdir versions')
+
+    extract = local('tar -cvzf versions/{} web_static/'.format(archive_path))
+    if extract.succeeded:
+        size = os.path.getsize('versions/{}'.format(archive_path))
+        print('web_static packed: {} -> {}Bytes'.format(archive_path, size))
+        return archive_path
+    else:
+        return None
