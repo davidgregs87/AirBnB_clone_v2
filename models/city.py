12383e7975bb12812b8f82fd637be7501e3a44c6
davidgregs87 <udohdavid27@gmail.com>
2023-07-24 11:41:07 +0100

    fixed all except Many to Many relationships

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+fr
 from os import getenv
 
 
@@ -19,8 +20,7 @@ class City(BaseModel, Base):
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities",
-                              cascade="all, delete-orphan")
+        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
     else:
         name = ""
         state_id = ""
